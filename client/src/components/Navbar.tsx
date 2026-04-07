2913feb8a72d642bd6fc3f87749d330490d4234d
andamho <83113420+andamho@users.noreply.github.com>
2026-04-07 13:05:16 +0900

    Fix: 새 로고 비율 유지 (w-auto로 가로 늘어남 수정)

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -398,7 +398,7 @@ export function Navbar() {
                   ref={imgRef}
                   src={logoLoaded ? logoImage : LOGO_TINY} 
                   alt="한국이름학교 로고" 
-                  className="h-[84px] w-[84px] md:h-[96px] md:w-[96px] -ml-[17px] -mr-[13px] md:-ml-[15px] md:-mr-[11px]"
+                  className="h-[84px] w-auto md:h-[96px] object-contain"
                   loading="eager"
                   decoding="sync"
                   onLoad={() => {
